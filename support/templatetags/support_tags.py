d65099dd0ddd2f13d9526d66cd578dd6960357f8
Philipp Wassibauer <phil@philmac.lan>
2010-08-06 10:50:22 +0200

    tab added

--- support/templatetags/support_tags.py
+++ support/templatetags/support_tags.py
@@ -4,11 +4,10 @@ from support.forms import ContactForm
 register = template.Library()
 
 
-def show_contact_form():
+def show_support_form():
     return {"contact_form": ContactForm()}
 register.inclusion_tag("support/contact_form.html")(show_contact_form)
 
-
-def show_feedback_form():
-    return {"contact_form": ContactForm()}
-register.inclusion_tag("support/feedback_form.html")(show_feedback_form)
+def show_support_tab():
+    return {}
+register.inclusion_tag("support/feedback_tab.html")(show_support_tab)
--- support/urls.py
+++ support/urls.py
@@ -8,5 +8,5 @@ from views import *
 urlpatterns = patterns('',
    url(r'^$',contact_form,name='contact_form'),
    url(r'^modular$', contact_form, {"template_name":"support/modal_form.html"}, name='modular_feedback'),
-   url(r'^sent/$', direct_to_template,{ 'template': 'support/contact_form_sent.html' },name='contact_form_sent'),
+   url(r'^success/$', direct_to_template, { 'template': 'support/contact_form_sent.html' },name='contact_form_sent'),
 )
